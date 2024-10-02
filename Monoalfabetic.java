b6a45e923484cf2d466b2351c63154477b3c45e9
Dani <thos.merida.dani@gmail.com>
2024-10-02 20:46:00 +0200

    continuació exercici Monoalfabetic

--- Monoalfabetic.java
+++ Monoalfabetic.java
@@ -6,31 +6,57 @@ import java.util.ArrayList;
 
 public class Monoalfabetic {
     public final char[] abecedari = "aáàäbcçdeéèëfghiíìïjklmnñoóòöpqrstuúùüvwxyz".toCharArray();
-    public final char[] abecedariMaj = "AÁÀÄBCÇDEÉÈËFGHIÍÌÏJKLMNÑOÓÒÖPQRSTUÚÙÜVXYZ".toCharArray();
-    
+
     public static void main(String[] args) {
         System.out.println("Introdueix una cadena per a xifrar:");
         Scanner scanner = new Scanner(System.in);
-        String paraula = scanner.nextLine();
-        System.out.println("Introdueix un número per desplaçar el caràcter");
-        int des = Integer.parseInt(scanner.nextLine());
-        RotX rotx = new RotX();
-        paraula = rotx.xifraRotX(paraula, des);
-        System.out.println(paraula);
-        rotx.forcaBrutaRotX(paraula);
+        String cadena = scanner.nextLine();
+        Monoalfabetic mono = new Monoalfabetic();
+        char [] mezclado = mono.permutaAlfabet();
+        
     }
 
-    public String xifraRotX(String cadena, int des) {
-        StringBuffer resultat = new StringBuffer();
+    public char[] permutaAlfabet(){
+        List<Character> permutat = new ArrayList();
+        for (char i: abecedari){
+            permutat.add(i);
+        }
+        Collections.shuffle(permutat);
+        char [] llistaFinal = new char[permutat.size()];
+        for (int j = 0; j < permutat.size(); j++){
+            llistaFinal[j] = permutat.get(j);
+        }
+        return llistaFinal;
+    }
+
+    public String xifraMonoAlfa(String cadena, char[] mezclado){
+        StringBuilder resultat = new StringBuilder();
+        for (char i: cadena.toCharArray()){
+            char ch = cadena.charAt(i);
+            int pos = i;
+            boolean maj = false;
+            if (Character.isLetter(i)){
+                if (Character.isUpperCase(i)) {
+                    maj = true;
+                    for (int j = 0; j < mezclado.length;j++){
+                        
+                    }
+                }
+            }
+            else{
+                resultat.append(i);
+            }
+        }
+        return resultat.toString();
+    }
+    /* public String xifraRotX(String cadena, int des) {
+        StringBuilder resultat = new StringBuilder();
         for (char i : cadena.toCharArray()) {
             if (Character.isLetter(i)) {
                 if (Character.isUpperCase(i)) {
                     for (int j = 0; j < abecedariMaj.length; j++) {
                         if (abecedariMaj[j] == i) {
-                            int pos = j + des;
-                            if (pos >= abecedariMaj.length) {
-                                pos -= abecedariMaj.length;
-                            }
+                            int pos = (j + des) % abecedariMaj.length;
                             resultat.append(abecedariMaj[pos]);
                             break;
                         }
@@ -38,10 +64,7 @@ public class Monoalfabetic {
                 } else {
                     for (int j = 0; j < abecedari.length; j++) {
                         if (abecedari[j] == i) {
-                            int pos = j + des;
-                            if (pos >= abecedari.length) {
-                                pos -= abecedari.length;
-                            }
+                            int pos = (j + des) % abecedari.length;
                             resultat.append(abecedari[pos]);
                             break;
                         }
@@ -52,19 +75,16 @@ public class Monoalfabetic {
             }
         }
         return resultat.toString();
-    }
+    } */
 
-    public String desxifraRotX(String cadena, int des) {
-        StringBuffer resultat = new StringBuffer();
+    /* public String desxifraRotX(String cadena, int des) {
+        StringBuilder resultat = new StringBuilder();
         for (char i : cadena.toCharArray()) {
             if (Character.isLetter(i)) {
                 if (Character.isUpperCase(i)) {
                     for (int j = 0; j < abecedariMaj.length; j++) {
                         if (abecedariMaj[j] == i) {
-                            int pos = j - des;
-                            if (pos < 0) {
-                                pos += abecedariMaj.length;
-                            }
+                            int pos = (j - des + abecedariMaj.length) % abecedariMaj.length;
                             resultat.append(abecedariMaj[pos]);
                             break;
                         }
@@ -72,10 +92,7 @@ public class Monoalfabetic {
                 } else {
                     for (int j = 0; j < abecedari.length; j++) {
                         if (abecedari[j] == i) {
-                            int pos = j - des;
-                            if (pos < 0) {
-                                pos += abecedariMaj.length;
-                            }
+                            int pos = (j - des + abecedari.length) % abecedari.length;
                             resultat.append(abecedari[pos]);
                             break;
                         }
@@ -86,12 +103,5 @@ public class Monoalfabetic {
             }
         }
         return resultat.toString();
-    }
-
-    public void forcaBrutaRotX(String cadena) {
-        for (int k = 0; k < abecedari.length; k++) {
-            System.out.println("Desplaçament " + k + ": " + desxifraRotX(cadena, k));
-        }
-    }
+    } */
 }
-
