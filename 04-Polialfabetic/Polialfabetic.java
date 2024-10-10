bf0824f593b2583db70bfd7d619483f3e893c9df
Daniel Merida <thos.merida.dani@gmail.com>
2024-10-10 14:52:13 +0200

    inici polialfabetic

--- 04-Polialfabetic/Polialfabetic.java
+++ 04-Polialfabetic/Polialfabetic.java
@@ -1,4 +1,3 @@
-
 import java.util.Collections;
 import java.util.List;
 import java.util.ArrayList;
@@ -11,68 +10,72 @@ public class Polialfabetic {
 
     public static void main(String[] args) {
         Polialfabetic poli = new Polialfabetic();
-        int clauSecreta = 532747258;
+        int clauSecreta = 532747258;  
         String msgs[] = { "Test 01 àrbritre, coixí, Perímetre",
-                "Test 02 Taüll, DÍA, año",
+                "Test 02 Taüll, DÍA, any",
                 "Test 03 Peça, Òrrius, Bòvila" };
         String msgsXifrats[] = new String[msgs.length];
 
         System.out.println("Xifratge:\n--------");
         for (int i = 0; i < msgs.length; i++) {
-            poli.initRandom(clauSecreta);
-            msgsXifrats[i] = poli.xifraPoliAlfa(msgs[i]);
+            poli.initRandom(clauSecreta);  
+            msgsXifrats[i] = poli.xifraPoliAlfa(msgs[i]);  
             System.out.printf("%-34s -> %s%n", msgs[i], msgsXifrats[i]);
         }
 
         System.out.println("Desxifratge:\n-----------");
         for (int i = 0; i < msgs.length; i++) {
-            poli.initRandom(clauSecreta);
-            String msg = poli.desxifraPoliAlfa(msgsXifrats[i]);
+            poli.initRandom(clauSecreta);  
+            String msg = poli.desxifraPoliAlfa(msgsXifrats[i]);  
             System.out.printf("%-34s -> %s%n", msgsXifrats[i], msg);
         }
     }
 
+    // Inicialitza el random amb el valor que agafem com a parametre 
     public void initRandom(int contrasenya) {
         ran = new Random(contrasenya);
     }
 
+    // Permuta l'alfabet utilitzant el random (ran)
     public void permutaAlfabet() {
         List<Character> mezclar = new ArrayList<Character>();
         for (char i : abecedari) {
             mezclar.add(i);
         }
-        Collections.shuffle(mezclar, ran);
+        Collections.shuffle(mezclar, ran);  
         char[] llistaFinal = new char[mezclar.size()];
         for (int j = 0; j < mezclar.size(); j++) {
             llistaFinal[j] = mezclar.get(j);
         }
-        permutat = llistaFinal;
+        permutat = llistaFinal;  
     }
 
     public String xifraPoliAlfa(String msg) {
-        return xifraCadena(msg, true);
+        return xifraCadena(msg, true);  
     }
 
     public String desxifraPoliAlfa(String msgXifrat) {
-        return xifraCadena(msgXifrat, false);
+        return xifraCadena(msgXifrat, false);  
     }
 
     public String xifraCadena(String cadena, boolean xifra) {
         StringBuilder resultat = new StringBuilder();
-        char[] base = permutat;
-        char[] desti = abecedari;
-        if (!xifra) {
-            base = abecedari;
-            desti = permutat;
-        }
         for (char i : cadena.toCharArray()) {
-            permutaAlfabet();
+            // Permuta l'alfabet per cada lletra
+            permutaAlfabet();  
+            char[] base = permutat;
+            char[] desti = abecedari;
+            if (!xifra) {
+                base = abecedari;
+                desti = permutat;
+            }
             int pos = i;
             boolean maj = false;
             if (Character.isLetter(i)) {
                 if (Character.isUpperCase(i)) {
+                    // Converteix a minúscula si és majúscula i posem com a valor true a la variable maj
                     maj = true;
-                    i = Character.toLowerCase(i);
+                    i = Character.toLowerCase(i);  
                 }
                 for (int j = 0; j < base.length; j++) {
                     if (base[j] == i) {
@@ -81,8 +84,9 @@ public class Polialfabetic {
                     }
                 }
                 char caracterXifrat = desti[pos];
+                // Torna a convertir en majúscula si la variable maj es true
                 if (maj) {
-                    caracterXifrat = Character.toUpperCase(caracterXifrat);
+                    caracterXifrat = Character.toUpperCase(caracterXifrat);  
                 }
                 resultat.append(caracterXifrat);
             } else {
